e4d16780b1059a71c6ee780c25b9974c75ae2875
OzaRu <114357633+0zaRu@users.noreply.github.com>
2023-05-11 18:54:55 +0200

    bins-modified
    todavía no sé por qué siempre se cambian, tengo que investigarlo

--- Tema 12/src/Proce_Almacenado.java
+++ Tema 12/src/Proce_Almacenado.java
@@ -33,8 +33,8 @@ public class Proce_Almacenado {
 			
 			//Creamos la sentencia y la metemos en la PreparedStatement pstm, parametrizada
 			pstm = con.prepareCall("( call PA_insertaAsignatura(?, ?) )");
-			pstm.setString("nomAsig", "Bases de Datos");
-			pstm.setInt("horasAsig", 6);
+			//pstm.setString("nomAsig", "Bases de Datos");
+			//pstm.setInt("horasAsig", 6);
 			pstm.execute();
 			
 			System.out.println("Fin del procedimientoo");
